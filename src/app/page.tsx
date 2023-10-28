36d49037ca82bf2031e9876a9610ebe243928897
rqphy <raphael.ferreira@hetic.net>
2023-10-28 11:35:14 +0100

    :sparkles: display all the players

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import styles from "./css/page.module.css"
+import playersList from "./data/players.json"
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<section className={styles.map}>test</section>
+			<section className={styles.map}>
+				{playersList.map((player) => (
+					<li className={styles.player}>{player}</li>
+				))}
+			</section>
 		</main>
 	)
 }

8821979f9efbf8c2626858ed126b234e93adb257
rqphy <raphael.ferreira@hetic.net>
2023-11-01 11:01:19 +0000

    :sparkles: setup orbit controls

--- app/_components/Experience/Experience.tsx
+++ app/_components/Experience/Experience.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React from "react"
 import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
 const Experience = () => {
 	return (
 		<Canvas>
+			<OrbitControls />
 			<mesh>
 				<torusKnotGeometry />
 				<meshNormalMaterial />

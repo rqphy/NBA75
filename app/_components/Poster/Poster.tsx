05502514b39aae9cba215ad7643903e9573d827e
rqphy <raphael.ferreira@hetic.net>
2023-11-27 19:37:17 +0100

    :sparkles::art: complete player card style

--- app/_components/Poster/Poster.tsx
+++ app/_components/Poster/Poster.tsx
@@ -1,10 +1,20 @@
-import { DoubleSide } from "three"
+import { useLoader } from "@react-three/fiber"
+import { group } from "console"
+import { DoubleSide, TextureLoader } from "three"
 
-export default function Poster() {
+export default function Poster({ posterLink = "/posters/stephen_curry.jpg" }) {
+    const [image] = useLoader(TextureLoader, [posterLink])
+
     return (
-        <mesh>
-            <planeGeometry args={[0.8, 1]} />
-            <meshNormalMaterial side={DoubleSide} />
-        </mesh>
+        <group>
+            <mesh position={[0, 0, -0.0001]}>
+                <planeGeometry args={[0.8, 1]} />
+                <meshBasicMaterial color={"black"} side={DoubleSide} />
+            </mesh>
+            <mesh>
+                <planeGeometry args={[0.8, 1]} />
+                <meshBasicMaterial map={image} />
+            </mesh>
+        </group>
     )
 }

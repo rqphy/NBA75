db687526a3f5b0cb8873ef9e5e4d2d947b9e518a
rqphy <raphael.ferreira@hetic.net>
2023-11-29 20:21:04 +0100

    :sparkles: add content on player page

--- app/player/[player_id]/page.tsx
+++ app/player/[player_id]/page.tsx
@@ -1,17 +1,27 @@
+import styles from "../../css/player.module.css"
 import { promises as fs } from "fs"
-
 interface pageProps {
-	params: { player_id: string }
+    params: { player_id: string }
 }
 
 export default async function page({ params }: pageProps) {
-	const file = await fs.readFile(
-		process.cwd() + "/app/data/players.json",
-		"utf8"
-	)
+    const file = await fs.readFile(
+        process.cwd() + "/app/data/players.json",
+        "utf8"
+    )
 
-	const data = JSON.parse(file)
-	const player = data[Number(params.player_id)]
+    const data = JSON.parse(file)
+    const player = data[Number(params.player_id)]
 
-	return <h1>{player.name}</h1>
+    return (
+        <section className={styles.player}>
+            <div className={styles.container}>
+                <h1 className={styles.title}>{player.name}</h1>
+                <figure className={styles.banner}>
+                    <img src="/banners/stephen_curry.jpg" alt={player.name} />
+                </figure>
+                <p>{player.story}</p>
+            </div>
+        </section>
+    )
 }

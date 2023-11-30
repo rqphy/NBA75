2f25bdc79fd6bebb542dfb95322854edca6a6d8d
rqphy <raphael.ferreira@hetic.net>
2023-11-30 17:08:20 +0100

    :construction: update switcher

--- app/_components/Switch/Switch.tsx
+++ app/_components/Switch/Switch.tsx
@@ -1,7 +1,14 @@
+import styles from "./Switch.module.css"
 interface switchProps {
     currentId: string
 }
 
 export default function Switch({ currentId }: switchProps) {
-    return <div>{currentId}</div>
+    return (
+        <ul className={styles.switch}>
+            <li className={styles.prev}>previous player</li>
+            <div className={styles.bball}></div>
+            <li className={styles.next}>next player</li>
+        </ul>
+    )
 }

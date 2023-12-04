1f4d8b0de8069abee79ac141838dead1524d162b
rqphy <raphael.ferreira@hetic.net>
2023-12-04 13:43:33 +0100

    :construction: switch -> make own link

--- app/_components/Switch/Switch.tsx
+++ app/_components/Switch/Switch.tsx
@@ -1,31 +1,41 @@
+"use client"
+import { useRouter } from "next/navigation"
 import styles from "./Switch.module.css"
 import Link from "next/link"
 interface switchProps {
-    currentId: number
+	currentId: number
 }
 
 export default function Switch({ currentId }: switchProps) {
-    return (
-        <ul className={styles.switch}>
-            {currentId > 0 && (
-                <Link href={`/player/${currentId - 1}`} className={styles.prev}>
-                    <figure>
-                        <img src="/arrow_icon.png" alt="previous player" />
-                    </figure>
-                </Link>
-            )}
-            <Link href={"/"} className={styles.bball}>
-                <figure>
-                    <img src="/bball.png" alt="basketball icon" />
-                </figure>
-            </Link>
-            {currentId < 75 && (
-                <Link href={`/player/${currentId + 1}`} className={styles.next}>
-                    <figure>
-                        <img src="/arrow_icon.png" alt="next player" />
-                    </figure>
-                </Link>
-            )}
-        </ul>
-    )
+	const router = useRouter()
+
+	const handleSwitchClick = (index: number) => {
+		router.push(`/player/${index}`)
+	}
+
+	return (
+		<ul className={styles.switch}>
+			{currentId > 0 && (
+				<figure
+					onClick={() => handleSwitchClick(currentId - 1)}
+					className={styles.prev}
+				>
+					<img src="/arrow_icon.png" alt="previous player" />
+				</figure>
+			)}
+			<Link href={"/"} className={styles.bball}>
+				<figure>
+					<img src="/bball.png" alt="basketball icon" />
+				</figure>
+			</Link>
+			{currentId < 75 && (
+				<figure
+					onClick={() => handleSwitchClick(currentId + 1)}
+					className={styles.next}
+				>
+					<img src="/arrow_icon.png" alt="next player" />
+				</figure>
+			)}
+		</ul>
+	)
 }

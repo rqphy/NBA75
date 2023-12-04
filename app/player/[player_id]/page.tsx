32306e958729b288aea8a495671b6eef6d0ff01f
rqphy <raphael.ferreira@hetic.net>
2023-12-04 13:34:23 +0100

    :dizzy: add page in transition

--- app/player/[player_id]/page.tsx
+++ app/player/[player_id]/page.tsx
@@ -2,33 +2,34 @@ import styles from "../../css/player.module.css"
 import { promises as fs } from "fs"
 import Switch from "@/app/_components/Switch/Switch"
 interface pageProps {
-    params: { player_id: string }
+	params: { player_id: string }
 }
 
 export default async function page({ params }: pageProps) {
-    const file = await fs.readFile(
-        process.cwd() + "/app/data/players.json",
-        "utf8"
-    )
+	const file = await fs.readFile(
+		process.cwd() + "/app/data/players.json",
+		"utf8"
+	)
 
-    const data = JSON.parse(file)
-    const player = data[Number(params.player_id)]
+	const data = JSON.parse(file)
+	const player = data[Number(params.player_id)]
 
-    return (
-        <>
-            <section className={styles.player}>
-                <div className={styles.container}>
-                    <h1 className={styles.title}>{player.name}</h1>
-                    <figure className={styles.banner}>
-                        <img
-                            src="/banners/stephen_curry.jpg"
-                            alt={player.name}
-                        />
-                    </figure>
-                    <p className={styles.story}>{player.story}</p>
-                </div>
-            </section>
-            <Switch currentId={Number(params.player_id)} />
-        </>
-    )
+	return (
+		<>
+			<div className={styles.transition}></div>
+			<section className={styles.player}>
+				<div className={styles.container}>
+					<h1 className={styles.title}>{player.name}</h1>
+					<figure className={styles.banner}>
+						<img
+							src="/banners/stephen_curry.jpg"
+							alt={player.name}
+						/>
+					</figure>
+					<p className={styles.story}>{player.story}</p>
+				</div>
+			</section>
+			<Switch currentId={Number(params.player_id)} />
+		</>
+	)
 }

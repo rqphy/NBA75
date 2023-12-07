04eeba15cc6cdce6180a3dfe00859d767b4c414a
rqphy <raphael.ferreira@hetic.net>
2023-12-07 20:30:37 +0100

    :bug: fix json path

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Experience from "./_components/Experience/Experience"
 import { promises as fs } from "fs"
 
 export default async function Home() {
-	const file = await fs.readFile(process.cwd() + "/players.json", "utf8")
+	const file = await fs.readFile("/players.json", "utf8")
 
 	const data = JSON.parse(file)
 

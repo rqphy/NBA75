ab841182c3682def0a865d8f618e458568c38d01
rqphy <raphael.ferreira@hetic.net>
2023-12-11 21:58:05 +0100

    :bento: updat banner name

--- app/player/[player_id]/page.tsx
+++ app/player/[player_id]/page.tsx
@@ -4,28 +4,25 @@ import Switch from "@/app/_components/Switch/Switch"
 import playerList from "@/app/data/players.json"
 
 interface pageProps {
-	params: { player_id: string }
+    params: { player_id: string }
 }
 
 export default async function page({ params }: pageProps) {
-	const player = playerList[Number(params.player_id)]
+    const player = playerList[Number(params.player_id)]
 
-	return (
-		<>
-			<div className={styles.transition}></div>
-			<section className={styles.player}>
-				<div className={styles.container}>
-					<h1 className={styles.title}>{player.name}</h1>
-					<figure className={styles.banner}>
-						<img
-							src="/banners/stephen_curry.jpg"
-							alt={player.name}
-						/>
-					</figure>
-					<p className={styles.story}>{player.story}</p>
-				</div>
-			</section>
-			<Switch currentId={Number(params.player_id)} />
-		</>
-	)
+    return (
+        <>
+            <div className={styles.transition}></div>
+            <section className={styles.player}>
+                <div className={styles.container}>
+                    <h1 className={styles.title}>{player.name}</h1>
+                    <figure className={styles.banner}>
+                        <img src="/banners/6781.jpg" alt={player.name} />
+                    </figure>
+                    <p className={styles.story}>{player.story}</p>
+                </div>
+            </section>
+            <Switch currentId={Number(params.player_id)} />
+        </>
+    )
 }

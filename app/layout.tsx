230f30ba342c37a159ad1b63b2c770f9afc9e919
rqphy <raphael.ferreira@hetic.net>
2023-12-18 17:21:34 +0100

    :bento: add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
     title: "NBA75",
     description: "NBA 75th Anniversary Team in a ThreeJs presentation by rqphy",
+    openGraph: {
+        images: ["/logo.png"],
+    },
 }
 
 export default function RootLayout({
